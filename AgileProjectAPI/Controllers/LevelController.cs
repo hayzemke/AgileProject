f1121cd770abe9f3a8abb0349f2eec0b9025edc0
hayzemke <hay.zemke@gmail.com>
2022-06-21 18:01:26 -0400

    idk

--- AgileProjectAPI/Controllers/LevelController.cs
+++ AgileProjectAPI/Controllers/LevelController.cs
@@ -22,24 +22,24 @@ public class LevelController : ControllerBase
     //typically the logger is intermediate, it will hit the endpoints and tell you when, and how you hit specific endpoints and errors that may occur. 
 
     //C* Create- 
-    
+
     //* Update - increase or decrease level
 
     private int _count = 0;
     private bool UpdateLevel(int id, ILogger _logger)
     {
-        if(_logger !=null)
+        if (_logger != null)
         {
             _count++;
             //^ see line 19
-            _logger= _count;
+            _logger = _count;
             //^^ uploads to DB
             .Add(_logger);
             return true;
         }
         else
         {
-            return false; 
+            return false;
         }
     }
     // private bool UpdateLevel(int id, ILogger _logger)
